31b71e900047e4f39c201086f26b7970e1594a66
ukrainian-bird84 <143319836+ukrainian-bird84@users.noreply.github.com>
2023-11-26 12:22:22 +0100

    Update task-1.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,5 @@
 function makeArray(firstArray, secondArray, maxLength) {
-  const newArray = [firstArray, secondArray];
+  const newArray = firstArray.concat(secondArray);
 
   if (newArray.length > maxLength) {
     return newArray.slice(0, maxLength);
